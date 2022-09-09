9bcee0170457c1f4eb6920df9bab30c1c5a1a06a
Michelle <mchllmglps@gmail.com>
2022-09-09 18:48:47 +0800

    Michelle added her name.

--- index.java
+++ index.java
@@ -1,6 +1,7 @@
 public class Names {
     public static void main(String[] args) {
         System.out.println("Rogina Rolloque");
-         System.out.println("Rowena Pial");
+        System.out.println("Rowena Pial");
+        System.out.println("Michelle G. Maglapus");
     }
 }
